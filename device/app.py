1523829dec5dfd8c48482e841b8ebf9f9d96d339
Khoa Tran <kttran2320@gmail.com>
2022-02-15 18:10:12 -0500

    set up cloud mongodb

--- device/app.py
+++ device/app.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
+from flask_mongoengine import MongoEngine
 
-
+db = "patient-monitor"
+db_pass = os.environ["db_pass"]
+URI = "mongodb+srv://khoa:{db_pass}@cluster0.bfh4g.mongodb.net/{db}?retryWrites=true&w=majority"
+db.connect(URI)
 app = Flask(__name__)
 CORS(app)
 
@@ -16,4 +20,4 @@ def home():
     return "hello world\n"
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
