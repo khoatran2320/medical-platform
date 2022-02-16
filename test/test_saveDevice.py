6884f78198eea3a13499649f13fa73e7897db2fe
Khoa Tran <kttran2320@gmail.com>
2022-02-15 22:40:59 -0500

    changed file structure

--- test/test_saveDevice.py
+++ test/test_saveDevice.py
@@ -1,6 +1,7 @@
-from app import db
+from mongoengine import connect
 from device.device_utilities import make_device_obj
 from Models.Device import Device
+
 
 def test_saveDevice():
     test_device = {
@@ -11,7 +12,15 @@ def test_saveDevice():
         "firmwareVersion": 1.2,
         "serialNumber": 12345
     }   
+    #db name
+    db_name = "patient-monitor"
 
+    #obtain user password stored in environment
+    db_pass = os.environ["db_pass"]
+
+    #databse URI
+    URI = "mongodb+srv://khoa:{}@cluster0.bfh4g.mongodb.net/{}?retryWrites=true&w=majority".format(db_pass, db_name)
+    db = connect(host=URI)
     #save device
     device = make_device_obj(test_device)
     device.save()
