45996e3c90bc092d50cdd8626bc9330e44e4353c
Khoa Tran <kttran2320@gmail.com>
2022-03-12 18:06:42 -0500

    hashed user passwords

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,8 +1,7 @@
 import bcrypt
-from datetime import datetime
 
 def encrypt_pass(pw):
-    pw = pw.encode('utf-8')
+    # pw = pw.encode('utf-8')
     hpw = bcrypt.hashpw(pw, bcrypt.gensalt())
     return hpw
     
