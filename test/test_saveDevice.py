b94648701868e95b3fb59dfb51384643b23a8a4b
Khoa Tran <kttran2320@gmail.com>
2022-03-12 13:37:26 -0500

    moved test db setup to fixture

--- test/test_saveDevice.py
+++ test/test_saveDevice.py
@@ -1,20 +1,13 @@
 import os
 from mongoengine import connect
 from src.Models.Device import Device as DeviceModel
+import pytest
 
 """
 test saving a device to the database. Connection to live db is unavailable due to IP filtering. Will have to manually enable global IP request for test to pass under GitHub Actions environment.
 """
-def test_saveDevice():
-    test_device = {
-        "id": "9012374",
-        "deviceType": "THERMOMETER",
-        "datePurchased": "02/12/2000",
-        "assignedUser": "Jon Wick",
-        "assigner": "John WIck",
-        "firmwareVersion": "12.123",
-        "serialNumber": 12345
-    }   
+@pytest.fixture()
+def database():
     #db name
     db_name = "patient-monitor"
 
@@ -24,6 +17,17 @@ def test_saveDevice():
     #databse URI
     URI = "mongodb+srv://khoa:{}@cluster0.bfh4g.mongodb.net/{}?retryWrites=true&w=majority".format(db_pass, db_name)
     db = connect(host=URI)
+
+def test_saveDevice(database):
+    test_device = {
+        "id": "9012374",
+        "deviceType": "THERMOMETER",
+        "datePurchased": "02/12/2000",
+        "assignedUser": "Jon Wick",
+        "assigner": "John WIck",
+        "firmwareVersion": "12.123",
+        "serialNumber": 12345
+    }   
     #save device
     device = DeviceModel()
     device.set(test_device)
