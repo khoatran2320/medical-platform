158b3fd34d32c75275c8be90f337c97af644ae5c
Khoa Tran <kttran2320@gmail.com>
2022-03-12 19:41:12 -0500

    added query messages

--- src/controllers/chatController.py
+++ src/controllers/chatController.py
@@ -6,7 +6,7 @@ from Response import Response
 from Models.Chat import Chat as ChatModel
 
 # Import parsers
-from parsers.chat import _chat_parser, _chat_id_parser
+from parsers.chat import _chat_parser, _chat_id_parser, _chat_user_parser, _chat_number_parser
 
 chat_ns = Namespace('chat', 'Chat methods')
 
@@ -112,3 +112,75 @@ class Message(Resource):
 
         except:
             return Response("Unable to delete message", status=400)
+
+
+@chat_ns.route('/user-sent-messages')
+class UserSentMessages(Resource):
+    """
+    Retrieves messages sent by a user
+    """
+    @chat_ns.doc(
+        parser=_chat_user_parser,
+        responses={
+            200: "Get messages successfully",
+            400: "Unable to get messages",
+        }
+    )
+    def get(self):
+        """Get user sent messages"""
+        data = _chat_user_parser.parse_args()
+        try:
+            messages = ChatModel.objects(fromUser=data['userId']).all().order_by("-timestamp")
+            messages = [message.json() for message in messages]
+            return Response({"message": "Get user messages successfully", "chats": messages}, status=200)
+        except:
+            return Response("Unable to get user messages", status=400)
+
+@chat_ns.route('/user-received-messages')
+class UserReceviedMessages(Resource):
+    """
+    Retrieves messages sent to a user
+    """
+    @chat_ns.doc(
+        parser=_chat_user_parser,
+        responses={
+            200: "Get messages successfully",
+            400: "Unable to get messages",
+        }
+    )
+    def get(self):
+        """Get user received messages"""
+        data = _chat_user_parser.parse_args()
+        try:
+            messages = ChatModel.objects(toUser=data['userId']).all().order_by("-timestamp")
+            messages = [message.json() for message in messages]
+            return Response({"message": "Get user messages successfully", "chats": messages}, status=200)
+        except:
+            return Response("Unable to get user messages", status=400)
+
+@chat_ns.route('/recent-messages')
+class RecentMessages(Resource):
+    """
+    Retrieves n most recent messages
+    """
+    @chat_ns.doc(
+        parser=_chat_number_parser,
+        responses={
+            200: "Get messages successfully",
+            400: "Unable to get messages",
+        }
+    )
+    def get(self):
+        """Get user received messages"""
+        data = _chat_number_parser.parse_args()
+        try:
+            messages = ChatModel.objects().all().order_by("-timestamp")
+            ret = []
+            for i in range(data['amount']):
+                if i < len(messages):
+                    ret.append(messages[i].json())
+            return Response({"message": "Get messages successfully", "chats": ret}, status=200)
+        except Exception as e:
+            print(e)
+            return Response("Unable to get messages", status=400)
+    
--- src/parsers/chat.py
+++ src/parsers/chat.py
@@ -41,3 +41,17 @@ _chat_id_parser.add_argument('id',
                             required=True,
                             location=('args','json'),
                             help="This field cannot be blank.")
+
+_chat_user_parser = reqparse.RequestParser()
+_chat_user_parser.add_argument('userId',
+                            type=str,
+                            required=True,
+                            location=('args','json'),
+                            help="This field cannot be blank.")
+
+_chat_number_parser = reqparse.RequestParser()
+_chat_number_parser.add_argument('amount',
+                            type=int,
+                            required=True,
+                            location=('args','json'),
+                            help="This field cannot be blank.")
